dadbbfe91589d30d7d6d0e0671fc6dbaba6ed86b
Nathan Mota <nathansmota@gmail.com>
2025-03-01 23:18:56 -0300

    :sparkles: creating a notice shipping

---
+++ app/components/noticeShipping/index.tsx
@@ -0,0 +1,8 @@
+export default function NoticeShipping() {
+    return (
+        <div className="bg-black dark:bg-white flex justify-center items-center p-2 text-sm font-medium
+         uppercase text-white dark:text-black">
+            <p>Frete Grátis Para Todo Brasil</p>
+        </div>
+    )
+}
