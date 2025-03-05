e5d7b551036c58497bfde0655f9403a08a22d25a
Nathan Mota <nathansmota@gmail.com>
2025-03-04 21:01:40 -0300

    :sparkles: feat: creating a section for a new products

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Carousel from "./components/carousel";
 import HighlightBanner from "./components/HighlightBanner";
 import { Navbar } from "./components/navbar";
+import NewProducts from "./components/newProducts";
 import NoticeShipping from "./components/noticeShipping";
 import ProductCarousel from "./components/productCarousel";
 import { SpecialCollections } from "./components/specialCollections";
@@ -14,6 +15,7 @@ export default function Home() {
       <ProductCarousel />
       <SpecialCollections />
       <HighlightBanner />
+      <NewProducts />      
     </div>
   );
 }
